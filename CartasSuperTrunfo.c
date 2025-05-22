2a72501815a02da0f5e9fdebbe8144d6416d076d
andrelrmatta <andreluisrmatta@gmail.com>
2025-05-22 15:32:03 +0000

    supertrunfodesafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,14 +3,42 @@
 int main() {
 
     char estado[10];
-    int populacao;
     char cidade[10];
+    char codigo[3];
+    float area;
+    float pib;
+    int pontos;
+    int populacao;
+
+
 
     printf("Digite o nome da cidade: ");
     scanf("%s" , cidade);
 
     printf("Digite o nome do estado: ");
     scanf("%s", estado);
+
+    printf("Digite o codigo da carta: ");
+    scanf("%s" , codigo);
+
+    printf("Digite o total da populacao: ");
+    scanf("%e" , &populacao);
+
+    printf("Digite a area em km²: ");
+    scanf("%e" , &area);
+
+    printf("Digite o pib: ");
+    scanf("%i", &pib);
+
+
+
+
+
+
+
+
+
+
 
 
 
